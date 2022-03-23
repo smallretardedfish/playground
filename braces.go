ce10f4732fd7312b904bd8ab1830fc0b126bc94c
smallretardedfish <hitriy.koliy@gmail.com>
2022-03-23 18:10:06 +0200

    initial commit

---
+++ braces.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"Playground/stack"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func IsBalanced(str string) bool {
+	stack := stack.NewStack[string]()
+	for _, item := range []byte(str) {
+		val := string(item)
+		var expected string
+		if val == "(" || val == "{" || val == "[" {
+			stack.Push(val)
+			continue
+		}
+		switch val {
+		case ")":
+			expected = "("
+		case "]":
+			expected = "["
+		case "}":
+			expected = "{"
+		}
+		if stack.Pop() != expected {
+			return false
+		}
+	}
+	return stack.IsEmpty()
+}
+
+func main() {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		buffer := make([]byte, 1024)
+		_, err := reader.Read(buffer)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Println(IsBalanced(string(buffer)))
+	}
+}
---
+++ stack/stack.go
@@ -0,0 +1,39 @@
+package stack
+
+import "fmt"
+
+type Stack[T any] struct {
+	S []T
+}
+
+func NewStack[T any]() *Stack[T] {
+	stack := make([]T, 0)
+	return &Stack[T]{S: stack}
+}
+func (s *Stack[T]) Push(val T) {
+	stack := s.S
+	stack = append([]T(stack), val)
+	s.S = stack
+}
+func (s *Stack[T]) Pop() T {
+	var res T
+	stack := s.S
+	if len([]T(stack)) > 0 {
+		res = stack[len([]T(stack))-1] // element to be popped
+		stack = stack[:len([]T(stack))-1]
+		s.S = stack
+	}
+	return res
+}
+func (s *Stack[T]) String() string {
+	stack := s.S
+	res := fmt.Sprint(stack)
+	return res
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	if len([]T(s.S)) > 0 {
+		return false
+	}
+	return true
+}
