1d215de4039402604f4a538fb1d8fb9a5ecc32a8
smallretardedfish <hitriy.koliy@gmail.com>
2022-03-31 16:54:15 +0300

    did some practice

--- main.go
+++ main.go
@@ -2,28 +2,43 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"reflect"
 )
 
-const (
-	_ = 1 << iota
-	UA
-	ENG
-	US
-)
+type Foo struct {
+	a int
+}
+
+var _ MyReader = (*Foo)(nil)
+
+func (f *Foo) Read([]byte) (int, error) {
+	return 0, nil
+}
+func (f *Foo) Foo() error {
+	return nil
+}
+
+type MyReader interface {
+	Read([]byte) (int, error)
+}
+type AnotherReader interface {
+	Read([]byte) (int, error)
+	Foo() error
+}
 
 func main() {
-	const NIGER int = 100
-	countryCodes := []int{UA, ENG, US, NIGER}
-	for _, code := range countryCodes {
-		switch code {
-		case UA:
-			fmt.Println("UKRAINE")
-		case ENG:
-			fmt.Println("ENGLAND")
-		case US:
-			fmt.Println("UNITED STATES")
-		default:
-			fmt.Println("WFT")
-		}
-	}
+	fmt.Println((map[int]*int64)(nil)[123])
+	var f = &Foo{a: 1}
+	var mr MyReader
+	var ar AnotherReader
+	ar = f
+	mr = ar
+	var empty interface{}
+	var r io.Reader
+
+	r = mr
+	fmt.Println(reflect.TypeOf(r), reflect.ValueOf(r))
+	fmt.Printf("%T - %v\n", empty, empty)
+	fmt.Printf("%T - %v\n", r, r)
 }
